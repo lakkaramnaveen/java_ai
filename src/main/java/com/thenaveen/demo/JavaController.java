802df3c5d7a7591dbce70da3b455999852f1d9e6
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-04-09 12:33:09 -0400

    first commit

---
+++ src/main/java/com/thenaveen/demo/JavaController.java
@@ -0,0 +1,59 @@
+package com.thenaveen.demo;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.image.ImageModel;
+import org.springframework.ai.openai.audio.speech.SpeechModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class JavaController {
+
+    private final ChatClient client;
+    private final SpeechModel speechModel;
+    private final ImageModel imageModel;
+    private final VectorStore vectorStore;
+
+    private final static String DIR_IN = "Users/naveenkumarlakkaram/files/in";
+    private final static String DIR_OUT = "Users/naveenkumarlakkaram/files/in";
+
+    public JavaController(ChatClient.Builder builder, SpeechModel speechModel, ImageModel imageModel, VectorStore vectorStore){
+        this.client = builder.build();
+        this.speechModel = speechModel;
+        this.imageModel = imageModel;
+        this.vectorStore = vectorStore;
+    }
+
+    @GetMapping
+    public String getString(@RequestParam(defaultValue = "what is the meaning of Life") String message){
+        return client.prompt().user(message).call().content();
+    }
+
+    @GetMapping("/response")
+    public ChatResponse getResponse(@RequestParam(defaultValue = "what is the meaning of Life") String message){
+        return client.prompt().user(message).call().chatResponse();
+    }
+
+    @GetMapping("/entity")
+    public MyResponse getEntity(@RequestParam(defaultValue = "what is the meaning of Life") String message){
+        return client.prompt().user(message).call().entity(MyResponse.class);
+    }
+
+    record MyResponse(String generationTokens, String promptTokens, String totalTokens, String text){ }
+
+    @GetMapping("/rag")
+    public String getResponseFromOurData(@RequestParam(defaultValue = "Airspeeds") String message){
+        // so called rag
+        return client.prompt().user(message).advisors(new QuestionAnswerAdvisor(vectorStore)).call().content();
+    }
+
+    @GetMapping("/template")
+    public String getResponseFromTemplate(@RequestParam String type, @RequestParam String topic){
+        return client.prompt().user(u -> u.text("Create a {type} about {topic}, limit to 100 words").param("type", type).param("topic", topic)).call().content();
+    }
+}
